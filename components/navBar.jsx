bc24b23020b393f6673941b8132ada7007f26ba1
Jerome Quah <me@jqnxy.xyz>
2021-10-31 20:28:55 +0800

    Update content

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,7 +1,7 @@
 export function navBarItem(props) {
   return (
     <a href={props.url}>
-      <div className="transition-all mr-3 shadow-md bg-white inline-block rounded-md px-5 py-2 hover:text-pink-200 hover:ring-pink-200 hover:ring-2 hover:ring-opacity-80">
+      <div className="transition-all mr-3 shadow-md bg-white inline-block rounded-md px-5 py-2 hover:text-pink-400 hover:ring-pink-400 hover:ring-2 hover:ring-opacity-80">
         {props.title}
       </div>
     </a>
