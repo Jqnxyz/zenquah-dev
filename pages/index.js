ec7fe9f93285ad1f67e3f1074dab76fb3a4e301d
Jerome Quah <me@jqnxy.xyz>
2021-11-01 00:55:02 +0800

    Update placeholder content

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,34 @@ export default function Home() {
                 </div>
               </a>
             </Link>
-            <div className="py-5 mb-5 rounded-lg bg-white shadow-md">
-              <div className="px-5 font-medium text-2xl">Placeholder Content</div>
-              <div className="px-5 mt-5 font-light break-words text-justify">Mice mew mew swipe at owner&apos;s legs lasers are tiny mice so be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day check cat door for ambush 10 times before coming in. Kitty time bite the neighbor&apos;s bratty kid, but found somthing move i bite it tail chase red laser dot touch water with paw then recoil in horror swipe at owner&apos;s legs. Making sure that fluff gets into the owner&apos;s eyes mmmmmmmmmeeeeeeeeooooooooowwwwwwww to pet a cat, rub its belly, endure blood and agony, quietly weep, keep rubbing belly yet bleghbleghvomit my furball really tie the room together cat mojo but step on your keyboard while you&apos;re gaming and then turn in a circle . You call this cat food bite the neighbor&apos;s bratty kid. Sit on the laptop meoooow so slap kitten brother with paw yet leave buried treasure in the sandbox for the toddlers white cat sleeps on a black shirt ptracy howl on top of tall thing. Attack feet cattt catt cattty cat being a cat but run up and down stairs give me attention or face the wrath of my claws but meow all night having their mate disturbing sleeping humans.</div>
-            </div>
-            <div className="py-5 mb-5 rounded-lg bg-white shadow-md">
-              <div className="px-5 font-medium text-2xl">Placeholder Content</div>
-              <div className="px-5 mt-5 font-light break-words text-justify">Mice mew mew swipe at owner&apos;s legs lasers are tiny mice so be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day check cat door for ambush 10 times before coming in. Kitty time bite the neighbor&apos;s bratty kid, but found somthing move i bite it tail chase red laser dot touch water with paw then recoil in horror swipe at owner&apos;s legs. Making sure that fluff gets into the owner&apos;s eyes mmmmmmmmmeeeeeeeeooooooooowwwwwwww to pet a cat, rub its belly, endure blood and agony, quietly weep, keep rubbing belly yet bleghbleghvomit my furball really tie the room together cat mojo but step on your keyboard while you&apos;re gaming and then turn in a circle . You call this cat food bite the neighbor&apos;s bratty kid. Sit on the laptop meoooow so slap kitten brother with paw yet leave buried treasure in the sandbox for the toddlers white cat sleeps on a black shirt ptracy howl on top of tall thing. Attack feet cattt catt cattty cat being a cat but run up and down stairs give me attention or face the wrath of my claws but meow all night having their mate disturbing sleeping humans.</div>
-            </div>
+            <Link href="/">
+              <a>
+                <div className="group py-5 mb-5 rounded-lg transition-all bg-white shadow-md hover:ring-pink-400 hover:ring-1 hover:ring-opacity-80">
+                  <div className="px-5 font-medium text-2xl">Cats Shall Rule The World</div>
+                  <div className="px-5 mt-5 font-light break-words text-justify">Cats are the world. Meow in empty rooms. Attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently meow going to catch the red dot today going to catch the red dot today drool use lap as chair purrrrrr. Enslave the hooman.</div>
+                  <div className="transition-colors text-right font-semibold pr-5 pt-5 group-hover:text-pink-400">Continue Reading →</div>
+                </div>
+              </a>
+            </Link>
+            <Link href="/id">
+              <a>
+                <div className="group py-5 mb-5 rounded-lg transition-all bg-white shadow-md hover:ring-pink-400 hover:ring-1 hover:ring-opacity-80">
+                  <div className="px-5 font-medium text-2xl">Pretty Alright Privacy</div>
+                  <img class="object-cover h-64 my-5 w-full" src="https://static.govtech.agency/share/zen_quah-pgp-og-R87Vmf.jpg" />
+                  <div className="px-5 mt-5 font-light break-words text-justify">Using PGP to verify my work, messages, emails, and the sort.</div>
+                  <div className="transition-colors text-right font-semibold pr-5 pt-5 group-hover:text-pink-400">Continue Reading →</div>
+                </div>
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <div className="group py-5 mb-5 rounded-lg transition-all bg-white shadow-md hover:ring-pink-400 hover:ring-1 hover:ring-opacity-80">
+                  <div className="px-5 font-medium text-2xl">Cats Shall Rule The World</div>
+                  <div className="px-5 mt-5 font-light break-words text-justify">Cats are the world. Meow in empty rooms. Attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently meow going to catch the red dot today going to catch the red dot today drool use lap as chair purrrrrr. Enslave the hooman.</div>
+                  <div className="transition-colors text-right font-semibold pr-5 pt-5 group-hover:text-pink-400">Continue Reading →</div>
+                </div>
+              </a>
+            </Link>
           </div>
           {sidebar()}
         </div>
