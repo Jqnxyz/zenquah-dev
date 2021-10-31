e8a4f8271eec3fa0ebaefad4ee5ee4b671ce463d
Jerome Quah <me@jqnxy.xyz>
2021-10-31 23:08:21 +0800

    Add color theme

--- components/shared/pageHeader.jsx
+++ components/shared/pageHeader.jsx
@@ -19,7 +19,11 @@ export function header(props = defaultHeader) {
             <meta property="og:title" content={props.title} key="title" />
             <meta property="og:description" content={props.description} />
             <meta property="og:image" content={props.image} />
+
+            <meta name="theme-color" content="#ffb5c6" />
+
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
