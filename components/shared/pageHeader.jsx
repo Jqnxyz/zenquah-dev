ac5f93e9c5f3aa5e4ed3b6e2a2bf84d763d97b15
Jerome Quah <me@jqnxy.xyz>
2021-10-31 21:20:46 +0800

    Fix class error

--- components/shared/pageHeader.jsx
+++ components/shared/pageHeader.jsx
@@ -5,7 +5,7 @@ export function header() {
     <div>
         <div className="block m-0 md:flex md:m-auto">
             <div className="w-auto py-0 text-center md:text-left md:py-5">
-                <img class="inline object-cover rounded-full w-52 h-52 m-auto md:mr-5 md:w-20 md:h-20" src="https://static.govtech.agency/share/zen_quah-profile_picture-square-small-HwnQD6.jpg" alt="Profile image"/>
+                <img className="inline object-cover rounded-full w-52 h-52 m-auto md:mr-5 md:w-20 md:h-20" src="https://static.govtech.agency/share/zen_quah-profile_picture-square-small-HwnQD6.jpg" alt="Profile image"/>
             </div>
             <div className="w-screen p-5 md:px-0 md:w-4/6 md:m-0">
                 <div className="font-medium text-4xl text-center md:text-left">Zen Quah</div>
