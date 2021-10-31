034a9ecca9fdb1b11cfa0e9482fc2481a33c865b
Jerome Quah <me@jqnxy.xyz>
2021-10-31 17:34:53 +0800

    Update js to jsx

--- pages/page_components/navBar.js
+++
@@ -1,8 +0,0 @@
-export function navBarItem(props) {
-  return (
-    <div class="mr-3 shadow-md bg-white inline-block rounded-md px-5 py-2 hover:ring-pink-200 hover:ring-2 hover:ring-opacity-80">
-      <a href={props.url}>{props.title}</a>
-    </div>
-  )
-}
-
---
+++ pages/page_components/navBar.jsx
@@ -0,0 +1,8 @@
+export function navBarItem(props) {
+  return (
+    <div className="mr-3 shadow-md bg-white inline-block rounded-md px-5 py-2 hover:ring-pink-200 hover:ring-2 hover:ring-opacity-80">
+      <a href={props.url}>{props.title}</a>
+    </div>
+  )
+}
+
