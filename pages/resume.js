230259cc7145b9bb379de6155f3b281ffd598389
Jerome Quah <me@jqnxy.xyz>
2023-04-24 00:59:40 +0800

    Fix client issue

--- pages/resume.js
+++ pages/resume.js
@@ -1,10 +1,14 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 
-export default function Home() {
+export default function Resume() {
   const router = useRouter()
-  router.push('https://static.govtech.agency/share/Zen-Quah-Resume-2023-04-ssuWe2.pdf')
-  router.push('/')
+
+  useEffect(() => {
+    router.push('https://static.govtech.agency/share/Zen-Quah-Resume-2023-04-ssuWe2.pdf');
+    router.push('/');
+  }, []);
 
   return (
     <div className="min-h-screen bg-white">
